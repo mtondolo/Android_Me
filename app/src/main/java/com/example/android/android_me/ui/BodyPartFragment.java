aae889b9a4ff2137800d3947f730235f9fcee49e
mtondolo <mtondolo@gmail.com>
2018-07-16 12:49:46 +0200

    feat: Communicate between activities

--- app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
+++ app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
@@ -1,6 +1,19 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.android.android_me.ui;
 
 import android.os.Bundle;
@@ -18,7 +31,7 @@ import java.util.List;
 
 public class BodyPartFragment extends Fragment {
 
-    // DONE (3) Create final Strings to store state information about the list of images and list index
+    // Final Strings to store state information about the list of images and list index
     public static final String IMAGE_ID_LIST = "image_ids";
     public static final String LIST_INDEX = "list_index";
 
@@ -41,7 +54,7 @@ public class BodyPartFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        //Load the saved state (the list of images and list index) if there is one
+        // Load the saved state (the list of images and list index) if there is one
         if (savedInstanceState != null) {
             mImageIds = savedInstanceState.getIntegerArrayList(IMAGE_ID_LIST);
             mListIndex = savedInstanceState.getInt(LIST_INDEX);
@@ -59,177 +72,14 @@ public class BodyPartFragment extends Fragment {
             // Set the image resource to the list item at the stored index
             imageView.setImageResource(mImageIds.get(mListIndex));
 
-            //DONE (1) Set a click listener on the image view and on a click increment the list index and set the image resource
+            // Set a click listener on the image view
             imageView.setOnClickListener(new View.OnClickListener() {
                 @Override
-                public void onClick(View v) {
-                    //Increment position as long as the index remains <= the size of the image ids list
+                public void onClick(View view) {
+                    // Increment position as long as the index remains <= the size of the image ids list
                     if (mListIndex < mImageIds.size() - 1) {
                         mListIndex++;
                     } else {
-                        // DONE (2) If you reach the end of a list of images, set the list index back to 0 (the first item in the list)
-                        //The end of the list has been reached, so return to beginning index
-                        mListIndex = 0;
-                    }
-                    // Set the image resource to the new list item
-                    imageView.setImageResource(mImageIds.get(mListIndex));
-                }
-
-            });
-
-        } else {
-            Log.v(TAG, "This fragment has a null list of image id's");
-        }
-
-        // Return the rootView
-        return rootView;
-    }
-
-    // Setter methods for keeping track of the list images this fragment can display and which image
-    // in the list is currently being displayed
-    public void setImageIds(List<Integer> imageIds) {
-        mImageIds = imageIds;
-    }
-
-    public void setListIndex(int index) {
-        mListIndex = index;
-    }
-
-    // DONE (4) Override onSaveInstanceState and save the current state of this fragment
-    @Override
-    public void onSaveInstanceState(Bundle currentState) {
-        currentState.putIntegerArrayList(IMAGE_ID_LIST, (ArrayList<Integer>) mImageIds);
-        currentState.putInt(LIST_INDEX, mListIndex);
-    }
-}
-||||||| (empty tree)
-=======
-package com.example.android.android_me.ui;
-
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-
-import com.example.android.android_me.R;
-import com.example.android.android_me.data.AndroidImageAssets;
-
-public class BodyPartFragment extends Fragment {
-
-
-    //Mandatory constructor for instantiating the fragment
-    public BodyPartFragment(){
-
-    }
-
-    /**
-     *Inflates and populates the fragment layout
-     **/
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-       //Inflate the fragment layout
-       View rootView = inflater.inflate(R.layout.fragment_body_part, container, false);
-
-        //Get a reference to the image view in the fragment layout
-        ImageView imageView = (ImageView) rootView.findViewById(R.id.body_parts_image_view);
-
-        //Set the image resource to display
-        imageView.setImageResource(AndroidImageAssets.getHeads().get(0));
-
-        //Return the root view
-        return rootView;
-
-    }
-}
->>>>>>> 6ed79b6... feat: Create fragment
-||||||| merged common ancestors
-=======
-/*
-* Copyright (C) 2017 The Android Open Source Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*  	http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
-
-package com.example.android.android_me.ui;
-
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-
-import com.example.android.android_me.R;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class BodyPartFragment extends Fragment {
-
-    // Final Strings to store state information about the list of images and list index
-    public static final String IMAGE_ID_LIST = "image_ids";
-    public static final String LIST_INDEX = "list_index";
-
-    // Tag for logging
-    private static final String TAG = "BodyPartFragment";
-
-    // Variables to store a list of image resources and the index of the image that this fragment displays
-    private List<Integer> mImageIds;
-    private int mListIndex;
-
-    /**
-     * Mandatory empty constructor for the fragment manager to instantiate the fragment
-     */
-    public BodyPartFragment() {
-    }
-
-    /**
-     * Inflates the fragment layout file and sets the correct resource for the image to display
-     */
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-        // Load the saved state (the list of images and list index) if there is one
-        if(savedInstanceState != null) {
-            mImageIds = savedInstanceState.getIntegerArrayList(IMAGE_ID_LIST);
-            mListIndex = savedInstanceState.getInt(LIST_INDEX);
-        }
-
-        // Inflate the Android-Me fragment layout
-        View rootView = inflater.inflate(R.layout.fragment_body_part, container, false);
-
-        // Get a reference to the ImageView in the fragment layout
-        final ImageView imageView = (ImageView) rootView.findViewById(R.id.body_part_image_view);
-
-        // If a list of image ids exists, set the image resource to the correct item in that list
-        // Otherwise, create a Log statement that indicates that the list was not found
-        if(mImageIds != null){
-            // Set the image resource to the list item at the stored index
-            imageView.setImageResource(mImageIds.get(mListIndex));
-
-            // Set a click listener on the image view
-            imageView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    // Increment position as long as the index remains <= the size of the image ids list
-                    if(mListIndex < mImageIds.size()-1) {
-                        mListIndex++;
-                    } else {
                         // The end of list has been reached, so return to beginning index
                         mListIndex = 0;
                     }
@@ -266,129 +116,5 @@ public class BodyPartFragment extends Fragment {
         currentState.putInt(LIST_INDEX, mListIndex);
     }
 
-
 }
->>>>>>> feat: Create and display masterlistfragment
-||||||| merged common ancestors
-=======
-/*
-* Copyright (C) 2017 The Android Open Source Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*  	http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
 
-package com.example.android.android_me.ui;
-
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-
-import com.example.android.android_me.R;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class BodyPartFragment extends Fragment {
-
-    // Final Strings to store state information about the list of images and list index
-    public static final String IMAGE_ID_LIST = "image_ids";
-    public static final String LIST_INDEX = "list_index";
-
-    // Tag for logging
-    private static final String TAG = "BodyPartFragment";
-
-    // Variables to store a list of image resources and the index of the image that this fragment displays
-    private List<Integer> mImageIds;
-    private int mListIndex;
-
-    /**
-     * Mandatory empty constructor for the fragment manager to instantiate the fragment
-     */
-    public BodyPartFragment() {
-    }
-
-    /**
-     * Inflates the fragment layout file and sets the correct resource for the image to display
-     */
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-        // Load the saved state (the list of images and list index) if there is one
-        if(savedInstanceState != null) {
-            mImageIds = savedInstanceState.getIntegerArrayList(IMAGE_ID_LIST);
-            mListIndex = savedInstanceState.getInt(LIST_INDEX);
-        }
-
-        // Inflate the Android-Me fragment layout
-        View rootView = inflater.inflate(R.layout.fragment_body_part, container, false);
-
-        // Get a reference to the ImageView in the fragment layout
-        final ImageView imageView = (ImageView) rootView.findViewById(R.id.body_part_image_view);
-
-        // If a list of image ids exists, set the image resource to the correct item in that list
-        // Otherwise, create a Log statement that indicates that the list was not found
-        if(mImageIds != null){
-            // Set the image resource to the list item at the stored index
-            imageView.setImageResource(mImageIds.get(mListIndex));
-
-            // Set a click listener on the image view
-            imageView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    // Increment position as long as the index remains <= the size of the image ids list
-                    if(mListIndex < mImageIds.size()-1) {
-                        mListIndex++;
-                    } else {
-                        // The end of list has been reached, so return to beginning index
-                        mListIndex = 0;
-                    }
-                    // Set the image resource to the new list item
-                    imageView.setImageResource(mImageIds.get(mListIndex));
-                }
-            });
-
-        } else {
-            Log.v(TAG, "This fragment has a null list of image id's");
-        }
-
-        // Return the rootView
-        return rootView;
-    }
-
-    // Setter methods for keeping track of the list images this fragment can display and which image
-    // in the list is currently being displayed
-
-    public void setImageIds(List<Integer> imageIds) {
-        mImageIds = imageIds;
-    }
-
-    public void setListIndex(int index) {
-        mListIndex = index;
-    }
-
-    /**
-     * Save the current state of this fragment
-     */
-    @Override
-    public void onSaveInstanceState(Bundle currentState) {
-        currentState.putIntegerArrayList(IMAGE_ID_LIST, (ArrayList<Integer>) mImageIds);
-        currentState.putInt(LIST_INDEX, mListIndex);
-    }
-
-
-}
->>>>>>> feat: Define communication interface
